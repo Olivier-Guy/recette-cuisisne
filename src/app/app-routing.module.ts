db2f802149b1cbb677b3dfd639e4769b26260bad
Olivier GUY <olivier.guy.7334@gmail.com>
2023-01-31 13:24:04 +0100

    Ajout recette

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,12 @@ import { LoginComponent } from './login-component/login-component.component';
 import { RecetteComponent } from './recette/recette.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent, pathMatch: 'full'},
+  {path: '', redirectTo: 'recettes', pathMatch: 'full'},
   {path: 'recettes', component: ListeRecetteComponent},
   {path: 'recette/:id', component: RecetteComponent},
 ];
+
+//{path: 'login', component: LoginComponent, pathMatch: 'full'},
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
